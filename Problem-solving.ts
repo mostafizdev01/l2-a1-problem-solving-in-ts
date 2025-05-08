8332d9e58185bce4548e10e6961df766fc44dadc
Hera <mostafizhera@gmail.com>
2025-05-08 22:16:26 +0600

    problem 1 is solved

---
+++ Problem-solving.ts
@@ -0,0 +1,12 @@
+{
+    function formatString(input: string, toUpper?:boolean):string{
+        if(input === "string" && toUpper === true){
+            return input.toUpperCase()
+        }else{
+            return input;
+        }
+    }
+
+    
+      
+}
